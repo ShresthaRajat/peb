992ed1302e752422e56c9907b471a4631d57c1bb
Rajat Shrestha <shrestharajat@hotmail.com>
2020-12-31 15:06:46 +0545

    fixxx

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+app = application
+
 @app.route('/')
 def hello_world():
     return 'Example app v1'
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4444)
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=4444)
