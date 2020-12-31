86e195d7ca578ff55ce4d5daebd22cfa3118ff9b
Rajat Shrestha <shrestharajat@hotmail.com>
2020-12-31 15:54:55 +0545

    version 2

--- application.py
+++ application.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
 application = Flask(__name__)
 
-@application.route('/')
-def hello_world():
+
+@application.route('/v1')
+def v1():
     return 'Example app v1'
+
+
+@application.route('/')
+def index():
+    return render_template('index.html')
