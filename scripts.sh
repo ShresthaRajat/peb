343f6a909ca2ef84fb51da9ad9f7009e3ff5d81a
Rajat Shrestha <shrestharajat@hotmail.com>
2021-01-18 12:45:14 +0545

    add scripts

--- scripts.sh
+++ scripts.sh
@@ -4,3 +4,12 @@ python -m virtualenv ~/.cache/pyvenv/peb
 source ~/.cache/pyvenv/peb/bin/activate
 pip install flask
 pip freeze > requirements.txt
+
+
+git init
+git remote add git@github.com:ShresthaRajat/peb.git
+git add .
+git comm-t -m "initial commit"
+
+
+export FLASK_APP="application"
